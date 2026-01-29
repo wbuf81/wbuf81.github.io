c8b0cb2d3efbab7fe37902955aa066d5bc8f4b7c
Wesley Bard <wes.bard@gmail.com>
2026-01-29 10:29:32 -0500

    Make grain effect darker

--- app/articles/ArticlesClient.tsx
+++ app/articles/ArticlesClient.tsx
@@ -79,7 +79,7 @@ export default function ArticlesClient({ articles }: { articles: Article[] }) {
           pointer-events: none;
           z-index: 0;
           background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noise'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noise)'/%3E%3C/svg%3E");
-          opacity: 0.12;
+          opacity: 0.18;
           animation: grain 3s steps(1) infinite;
         }
 
